df86ea1d6b56828788811167d2980c9f6e4a2293
Oziel Santos <ozielmaster@gmail.com>
2021-11-03 13:43:25 -0300

    Alterações na classe BeerController

--- src/main/java/one/digitalinnovation/beerstock/exception/BeerStockExceededException.java
+++ src/main/java/one/digitalinnovation/beerstock/exception/BeerStockExceededException.java
@@ -6,7 +6,9 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(HttpStatus.BAD_REQUEST)
 public class BeerStockExceededException extends Exception {
 
-    public BeerStockExceededException(Long id, int quantityToIncrement) {
+ 	private static final long serialVersionUID = 1L;
+
+	public BeerStockExceededException(Long id, int quantityToIncrement) {
         super(String.format("Beers with %s ID to increment informed exceeds the max stock capacity: %s", id, quantityToIncrement));
     }
 }
